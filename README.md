283ab9435beac2eda38b187cca93f3f007ccd323
nwunderly <nwunder.dev@gmail.com>
2024-01-15 02:28:20 +0000

    initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # starlette-lego
 LEGO OAuth2 library for Starlette and FastAPI
+
+LEGO API Documentation: https://api.identity.lego.com/swagger/index.html
---
+++ starlette_lego/__init__.py
@@ -0,0 +1,14 @@
+"""
+starlette-lego
+--------------
+LEGO OAuth2 library for Starlette and FastAPI.
+
+:copyright: (c) 2024 nwunderly
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "starlette-lego"
+__author__ = "nwunderly"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 nwunderly"
+__version__ = "0.1.0"
